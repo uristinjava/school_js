42d6cb2b411ad1e32b12da69229c3dbeb444b983
Mikhail <m-klikov@yandex.ru>
2023-03-11 23:09:58 +0300

    3 block

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    window.addEventListener('scroll', () => {
+        document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+
+    })
+
+})
+
